8fb9fddfaceccd9a10974b32d2cb5209b05165fd
akesh0805 <vchd4.texnolog@gmail.com>
2025-03-15 22:58:43 +0500

    first committ

--- WorldTimeCli/obj/Debug/net8.0/win-x64/WorldTimeCli.AssemblyInfo.cs
+++ WorldTimeCli/obj/Debug/net8.0/win-x64/WorldTimeCli.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldTimeCli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+add8b0ad2958f7a331822c8adfefd8ed2073e349")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldTimeCli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldTimeCli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
